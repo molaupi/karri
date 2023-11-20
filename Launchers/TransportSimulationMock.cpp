9db378e87146991d552ca8748079aec94870926d
Moritz Laupichler <moritz.laupichler@kit.edu>
2023-11-20 16:21:01 +0100

    Started on executable mocking transport simulation.

---
+++ Launchers/TransportSimulationMock.cpp
@@ -0,0 +1,225 @@
+/// ******************************************************************************
+/// MIT License
+///
+/// Copyright (c) 2023 Moritz Laupichler <moritz.laupichler@kit.edu>
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+/// ******************************************************************************
+
+
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+#include <csv.h>
+
+#include "Tools/CommandLine/CommandLineParser.h"
+#include "Tools/Logging/LogManager.h"
+#include "DataStructures/Graph/Graph.h"
+#include "DataStructures/Graph/Attributes/LatLngAttribute.h"
+#include "DataStructures/Graph/Attributes/EdgeIdAttribute.h"
+#include "DataStructures/Graph/Attributes/OsmNodeIdAttribute.h"
+#include "DataStructures/Graph/Attributes/FreeFlowSpeedAttribute.h"
+#include "DataStructures/Graph/Attributes/EdgeTailAttribute.h"
+#include "DataStructures/Graph/Attributes/TravelTimeAttribute.h"
+#include "DataStructures/Graph/Attributes/PsgEdgeToCarEdgeAttribute.h"
+#include "DataStructures/Graph/Attributes/CarEdgeToPsgEdgeAttribute.h"
+
+#include "Algorithms/KaRRi/InputConfig.h"
+#include "Algorithms/KaRRi/BaseObjects/Vehicle.h"
+#include "Algorithms/KaRRi/BaseObjects/Request.h"
+#include "Algorithms/KaRRi/PbnsAssignments/VehicleLocator.h"
+#include "Algorithms/KaRRi/EllipticBCH/FeasibleEllipticDistances.h"
+#include "Algorithms/KaRRi/EllipticBCH/EllipticBucketsEnvironment.h"
+#include "Algorithms/KaRRi/EllipticBCH/EllipticBCHSearches.h"
+#include "Algorithms/KaRRi/CostCalculator.h"
+#include "Algorithms/KaRRi/RequestState/RequestState.h"
+#include "Algorithms/KaRRi/RequestState/RelevantPDLocs.h"
+#include "Algorithms/KaRRi/PDDistanceQueries/PDDistances.h"
+#include "Algorithms/KaRRi/RequestState/RelevantPDLocsFilter.h"
+#include "Algorithms/KaRRi/OrdinaryAssignments/OrdinaryAssignmentsFinder.h"
+#include "Algorithms/KaRRi/PbnsAssignments/PBNSAssignmentsFinder.h"
+#include "Algorithms/KaRRi/PalsAssignments/PALSAssignmentsFinder.h"
+#include "Algorithms/KaRRi/DalsAssignments/DALSAssignmentsFinder.h"
+#include "Algorithms/KaRRi/LastStopSearches/SortedLastStopBucketsEnvironment.h"
+#include "Algorithms/KaRRi/LastStopSearches/UnsortedLastStopBucketsEnvironment.h"
+#include "Algorithms/KaRRi/RequestState/VehicleToPDLocQuery.h"
+#include "Algorithms/KaRRi/RequestState/RequestStateInitializer.h"
+#include "Algorithms/KaRRi/AssignmentFinder.h"
+#include "Algorithms/KaRRi/SystemStateUpdater.h"
+#include "Algorithms/KaRRi/EventSimulations/StandaloneEventSimulation.h"
+#include "Algorithms/KaRRi/EventSimulations/MobitoppEventSimulation.h"
+#include "Algorithms/KaRRi/EventSimulations/MobitoppCommunicator.h"
+
+#ifdef KARRI_USE_CCHS
+#include "Algorithms/KaRRi/CHEnvironments/CCHEnvironment.h"
+#else
+#include "Algorithms/KaRRi/CHEnvironments/CHEnvironment.h"
+#endif
+
+#if KARRI_PD_STRATEGY == KARRI_BCH_PD_STRAT
+
+#include "Algorithms/KaRRi/PDDistanceQueries/BCHStrategy.h"
+
+#else // KARRI_PD_STRATEGY == KARRI_CH_PD_STRAT
+#include "Algorithms/KaRRi/PDDistanceQueries/CHStrategy.h"
+#endif
+
+
+#if KARRI_PALS_STRATEGY == KARRI_COL
+
+#include "Algorithms/KaRRi/PalsAssignments/CollectiveBCHStrategy.h"
+
+#elif KARRI_PALS_STRATEGY == KARRI_IND
+
+#include "Algorithms/KaRRi/PalsAssignments/IndividualBCHStrategy.h"
+
+#else // KARRI_PALS_STRATEGY == KARRI_DIJ
+
+#include "Algorithms/KaRRi/PalsAssignments/DijkstraStrategy.h"
+
+#endif
+
+#if KARRI_DALS_STRATEGY == KARRI_COL
+
+#include "Algorithms/KaRRi/DalsAssignments/CollectiveBCHStrategy.h"
+
+#elif KARRI_DALS_STRATEGY == KARRI_IND
+
+#include "Algorithms/KaRRi/DalsAssignments//IndividualBCHStrategy.h"
+
+#else // KARRI_DALS_STRATEGY == KARRI_DIJ
+
+#include "Algorithms/KaRRi/DalsAssignments/DijkstraStrategy.h"
+
+#endif
+
+
+inline void printUsage() {
+    std::cout <<
+              "Usage: mockTransportSim -r <requests> -o <file>\n"
+              "Mocks the behavior of a transport simulation like MobiTopp that issues requests to KaRRi being used as\n"
+              "a fleet simulation engine. Simply reads requests from file like KaRRi would and sends them to KaRRi via\n"
+              "socket interface.\n"
+              "  -veh-g <file>          vehicle road network in binary format.\n"
+              "  -r <file>              requests in CSV format.\n"
+              "  -csv-in-LOUD-format    if set, assumes that input files are in the format used by LOUD.\n"
+              "  -o <file>              generate output files at name <file> (specify name without file suffix).\n"
+              "  -help                  show usage help text.\n";
+}
+
+int main(int argc, char *argv[]) {
+    using namespace karri;
+    try {
+        CommandLineParser clp(argc, argv);
+        if (clp.isSet("help")) {
+            printUsage();
+            return EXIT_SUCCESS;
+        }
+
+        const auto vehicleNetworkFileName = clp.getValue<std::string>("veh-g");
+        const auto requestFileName = clp.getValue<std::string>("r");
+        const bool csvFilesInLoudFormat = clp.isSet("csv-in-LOUD-format");
+        auto outputFileName = clp.getValue<std::string>("o");
+        if (endsWith(outputFileName, ".csv"))
+            outputFileName = outputFileName.substr(0, outputFileName.size() - 4);
+
+
+        LogManager<std::ofstream>::setBaseFileName(outputFileName + ".");
+
+        // Read the vehicle network from file.
+        std::cout << "Reading vehicle network from file... " << std::flush;
+        using VehicleVertexAttributes = VertexAttrs<LatLngAttribute, OsmNodeIdAttribute>;
+        using VehicleEdgeAttributes = EdgeAttrs<
+                EdgeIdAttribute, EdgeTailAttribute, FreeFlowSpeedAttribute, TravelTimeAttribute, CarEdgeToPsgEdgeAttribute, OsmRoadCategoryAttribute>;
+        using VehicleInputGraph = StaticGraph<VehicleVertexAttributes, VehicleEdgeAttributes>;
+        std::ifstream vehicleNetworkFile(vehicleNetworkFileName, std::ios::binary);
+        if (!vehicleNetworkFile.good())
+            throw std::invalid_argument("file not found -- '" + vehicleNetworkFileName + "'");
+        VehicleInputGraph vehicleInputGraph(vehicleNetworkFile);
+        vehicleNetworkFile.close();
+        std::vector<int32_t> vehGraphOrigIdToSeqId;
+        if (vehicleInputGraph.numEdges() > 0 && vehicleInputGraph.edgeId(0) == INVALID_ID) {
+            vehGraphOrigIdToSeqId.assign(vehicleInputGraph.numEdges(), INVALID_ID);
+            std::iota(vehGraphOrigIdToSeqId.begin(), vehGraphOrigIdToSeqId.end(), 0);
+            FORALL_VALID_EDGES(vehicleInputGraph, v, e) {
+                    assert(vehicleInputGraph.edgeId(e) == INVALID_ID);
+                    vehicleInputGraph.edgeTail(e) = v;
+                    vehicleInputGraph.edgeId(e) = e;
+                }
+        } else {
+            FORALL_VALID_EDGES(vehicleInputGraph, v, e) {
+                    assert(vehicleInputGraph.edgeId(e) != INVALID_ID);
+                    if (vehicleInputGraph.edgeId(e) >= vehGraphOrigIdToSeqId.size()) {
+                        const auto numElementsToBeInserted =
+                                vehicleInputGraph.edgeId(e) + 1 - vehGraphOrigIdToSeqId.size();
+                        vehGraphOrigIdToSeqId.insert(vehGraphOrigIdToSeqId.end(), numElementsToBeInserted, INVALID_ID);
+                    }
+                    assert(vehGraphOrigIdToSeqId[vehicleInputGraph.edgeId(e)] == INVALID_ID);
+                    vehGraphOrigIdToSeqId[vehicleInputGraph.edgeId(e)] = e;
+                    vehicleInputGraph.edgeTail(e) = v;
+                    vehicleInputGraph.edgeId(e) = e;
+                }
+        }
+        std::cout << "done.\n";
+
+        // Read the request data from file.
+        std::cout << "Reading request data from file... " << std::flush;
+        std::vector<Request> requests;
+        int origin, destination, issuingTime, numRiders;
+        io::CSVReader<4, io::trim_chars<' '>> reqFileReader(requestFileName);
+
+        if (csvFilesInLoudFormat) {
+            reqFileReader.read_header(io::ignore_missing_column, "pickup_spot", "dropoff_spot", "min_dep_time", "num_riders");
+        } else {
+            reqFileReader.read_header(io::ignore_missing_column, "origin", "destination", "req_time", "num_riders");
+        }
+
+        numRiders = -1;
+        while (reqFileReader.read_row(origin, destination, issuingTime, numRiders)) {
+            if (origin < 0 || origin >= vehGraphOrigIdToSeqId.size() || vehGraphOrigIdToSeqId[origin] == INVALID_ID)
+                throw std::invalid_argument("invalid location -- '" + std::to_string(origin) + "'");
+            if (destination < 0 || destination >= vehGraphOrigIdToSeqId.size() ||
+                vehGraphOrigIdToSeqId[destination] == INVALID_ID)
+                throw std::invalid_argument("invalid location -- '" + std::to_string(destination) + "'");
+//            if (numRiders > maxCapacity)
+//                throw std::invalid_argument("number of riders '" + std::to_string(numRiders) + "' is larger than max vehicle capacity (" + std::to_string(maxCapacity) + ")");
+            const auto originSeqId = vehGraphOrigIdToSeqId[origin];
+            assert(vehicleInputGraph.toPsgEdge(originSeqId) != CarEdgeToPsgEdgeAttribute::defaultValue());
+            const auto destSeqId = vehGraphOrigIdToSeqId[destination];
+            assert(vehicleInputGraph.toPsgEdge(destSeqId) != CarEdgeToPsgEdgeAttribute::defaultValue());
+            const int requestId = static_cast<int>(requests.size());
+            if (numRiders == -1) // If number of riders was not specified, assume one rider
+                numRiders = 1;
+            requests.push_back({requestId, originSeqId, destSeqId, numRiders, issuingTime * 10, issuingTime * 10});
+            numRiders = -1;
+        }
+        std::cout << "done.\n";
+
+
+        // todo: Iterate requests and send via socket
+
+    } catch (std::exception &e) {
+        std::cerr << argv[0] << ": " << e.what() << '\n';
+        std::cerr << "Try '" << argv[0] << " -help' for more information.\n";
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
